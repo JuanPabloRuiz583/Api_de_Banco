3bc0d070af719b373a1d7bab775d9385a8135f07
JuanPabloRuiz583 <juaruizdesouza@gmail.com>
2025-04-09 15:34:10 -0300

    Adicionando o swagger

--- src/main/java/br/com/fiap/ApiBank/ApiBankApplication.java
+++ src/main/java/br/com/fiap/ApiBank/ApiBankApplication.java
@@ -1,10 +1,14 @@
 package br.com.fiap.ApiBank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de um banco", description = "API de um banco digital, ligada com o banco de dados, que realiza transaçoes de deposito para uma conta, saque, pix e entre outras funcionalidades", version = "v1"))
 public class ApiBankApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBankApplication.class, args);
